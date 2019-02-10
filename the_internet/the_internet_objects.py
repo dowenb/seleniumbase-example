0d4d94eaf5256bb7a29a697f84ab253866265e4b
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2019-02-10 13:48:30 +0000

    add tests for checkboxes

--- the_internet/the_internet_objects.py
+++ the_internet/the_internet_objects.py
@@ -1,9 +1,22 @@
+def _url(path, prefix='http://'):
+    base_url = 'the-internet.herokuapp.com'
+    return prefix + base_url + path
+    # TODO get base URL from some sort of environment variable that can be set via CI
+
+
 class BasicAuth(object):
-    url = 'http://admin:admin@the-internet.herokuapp.com/basic_auth'
+    url = _url('/basic_auth', prefix='http://admin:admin@')
     success_text = '#content > div > p'
 
+
 class BrokenImages(object):
-    url = 'http://the-internet.herokuapp.com/broken_images'
+    url = _url('/broken_images')
     image1 = '#content > div > img:nth-child(2)'
     image2 = '#content > div > img:nth-child(3)'
     image3 = '#content > div > img:nth-child(4)'
+
+
+class CheckBoxes(object):
+    url = _url('/checkboxes')
+    checkbox1 = '#checkboxes > input[type="checkbox"]:nth-child(1)'
+    checkbox2 = '#checkboxes > input[type="checkbox"]:nth-child(3)'
--- the_internet/the_internet_test.py
+++ the_internet/the_internet_test.py
@@ -1,7 +1,14 @@
 from seleniumbase import BaseCase
-from the_internet_objects import BasicAuth, BrokenImages
+from the_internet_objects import BasicAuth, BrokenImages, CheckBoxes
 
 class MyTestClass(BaseCase):
+
+    def isChecked(self, checkBox):
+        try:
+            self.get_attribute(checkBox, 'checked')
+            return True
+        except:
+            return False
 
     def test_basic_auth(self):
         self.open(BasicAuth.url)
@@ -15,3 +22,14 @@ class MyTestClass(BaseCase):
         self.assertGreater(int(self.get_attribute(BrokenImages.image1, 'naturalWidth')), 0)
         self.assertGreater(int(self.get_attribute(BrokenImages.image2, 'naturalWidth')), 0)
         self.assertGreater(int(self.get_attribute(BrokenImages.image3, 'naturalWidth')), 0)
+    
+    def test_checkboxes(self):
+        self.open(CheckBoxes.url)
+        #checkbox1 should default unchecked, and a click should leave it checked
+        self.assert_false(self.isChecked(CheckBoxes.checkbox1), 'Checkbox1 should be unchecked')
+        self.click(CheckBoxes.checkbox1)
+        self.assert_true(self.isChecked(CheckBoxes.checkbox1), 'Checkbox1 should be unchecked')
+         #checkbox2 should default checked, and a click should leave it unchecked
+        self.assert_true(self.isChecked(CheckBoxes.checkbox2), 'Checkbox2 should be checked')
+        self.click(CheckBoxes.checkbox2)
+        self.assert_false(self.isChecked(CheckBoxes.checkbox2), 'Checkbox2 should be unchcked')
